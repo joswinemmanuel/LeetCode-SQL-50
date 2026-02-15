93aeba08f5f90d77cebbb524a4efa359af59dfe5
joswinemmanuel <joswinemmanuel2002@gmail.com>
2026-02-15 23:52:22 +0530

    Recyclable and Low Fat Products using Pandas

---
+++ 01_pandas_recyclable-and-low-fat-products.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = [
+    ['0', 'Y', 'N'],
+    ['1', 'Y', 'Y'],
+    ['2', 'N', 'Y'],
+    ['3', 'Y', 'Y'],
+    ['4', 'N', 'N']
+]
+
+products = pd.DataFrame(data, columns=['product_id', 'low_fats', 'recyclable']).astype(
+    {'product_id': 'int64', 'low_fats': 'category', 'recyclable': 'category'}
+)
+
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    mask = products['low_fats'].eq('Y') & products['recyclable'].eq('Y')
+    return products.loc[mask, ['product_id']]
+
+print(find_products(products))
