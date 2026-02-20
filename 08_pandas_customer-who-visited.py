11482aa00a7d758dc066f81381abf2ca554a5d43
joswinemmanuel <joswinemmanuel2002@gmail.com>
2026-02-20 13:52:36 +0530

    Customer Who Visited using Pandas

---
+++ 08_pandas_customer-who-visited.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+data = [
+    [1, 23],
+    [2, 9],
+    [4, 30],
+    [5, 54],
+    [6, 96],
+    [7, 54],
+    [8, 54]
+]
+visits = pd.DataFrame(data, columns=['visit_id', 'customer_id']).astype(
+    {'visit_id': 'Int64', 'customer_id': 'Int64'}
+)
+
+data = [
+    [2, 5, 310],
+    [3, 5, 300],
+    [9, 5, 200],
+    [12, 1, 910],
+    [13, 2, 970]
+]
+transactions = pd.DataFrame(data, columns=['transaction_id', 'visit_id', 'amount']).astype(
+    {'transaction_id': 'Int64', 'visit_id': 'Int64', 'amount': 'Int64'}
+)
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    merged = pd.merge(visits, transactions, how='left', on='visit_id')
+    no_transactions = merged[merged['transaction_id'].isna()]
+    grouped = no_transactions.groupby('customer_id')['visit_id'].count().reset_index(name='count_no_trans')
+    return grouped
+
+print(find_customers(visits, transactions))
