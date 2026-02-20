c8aa2ed20074c8f193a6fb6b3384dfd81e5cd3a1
joswinemmanuel <joswinemmanuel2002@gmail.com>
2026-02-20 13:23:16 +0530

    Customer Who Visited using SQL

---
+++ 08_sql_customer-who-visited.sql
@@ -0,0 +1,12 @@
+-- SELECT customer_id, count(*) AS 'count_no_trans'
+-- FROM Visits
+-- WHERE visit_id
+-- NOT IN (SELECT visit_id FROM Transactions)
+-- GROUP BY customer_id;
+
+SELECT v.customer_id, count(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
