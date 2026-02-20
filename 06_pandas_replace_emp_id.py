687735c03f9ba278fdcb694ced6a0bcc0560e3cd
joswinemmanuel <joswinemmanuel2002@gmail.com>
2026-02-20 12:10:39 +0530

    Replace Employee ID using SQL

---
+++ 06_pandas_replace_emp_id.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+data = [[1, 'Alice'], [7, 'Bob'], [11, 'Meir'],
+        [90, 'Winston'], [3, 'Jonathan']]
+employees = pd.DataFrame(data, columns=['id', 'name']).astype(
+    {'id': 'int64', 'name': 'object'})
+data = [[3, 1], [11, 2], [90, 3]]
+employee_uni = pd.DataFrame(data, columns=['id', 'unique_id']).astype({
+    'id': 'int64', 'unique_id': 'int64'})
+
+
+# def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+#     merged = pd.merge(employees, employee_uni, how='left', on='id')
+#     return merged.loc[:, ['unique_id', 'name']]
+
+
+def replace_employee_id(employee: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    merged = pd.merge(employee, employee_uni, how="left", on="id")
+    return merged.loc[:, ["unique_id", "name"]]
+
+
+print(replace_employee_id(employees, employee_uni))
