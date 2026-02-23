0caef719338bc64e56e0e12accace5c7278c3eb2
joswinemmanuel <joswinemmanuel2002@gmail.com>
2026-02-23 15:05:08 +0530

    Rising Temperature using Pandas

--- 09_pandas_rising-temperature.py
+++ 09_pandas_rising-temperature.py
@@ -12,18 +12,33 @@ weather = pd.DataFrame(data, columns=['id', 'recordDate', 'temperature']).astype
 )
 
 
+# def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+#     weather['prev_date'] = weather['recordDate'] - pd.Timedelta(days=1)
+
+#     merged = pd.merge(
+#         weather,
+#         weather,
+#         left_on='prev_date',
+#         right_on='recordDate',
+#         suffixes=('_today', '_yesterday')
+#     )
+
+#     result = merged[
+#         merged['temperature_today'] > merged['temperature_yesterday']
+#     ][['id_today']]
+
+#     return result.rename(columns={'id_today': 'Id'})
+
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    weather['prev_date'] = weather['recordDate'] - pd.Timedelta(days=1)
-    print(weather)
-    merged = pd.merge(
-        weather,
-        weather,
-        left_on='prev_date',
-        right_on='recordDate',
-        suffixes=('_today', '_yesterday')
-    )
-    
-    return merged
+    weather = weather.sort_values(by='recordDate')
+    weather['prev_date'] = weather['recordDate'].shift(1)
+    weather['prev_temperature'] = weather['temperature'].shift(1)
+    result = weather[
+        (weather['recordDate'] - pd.Timedelta(days=1) == weather['prev_date'])
+        &
+        (weather['temperature'] > weather['prev_temperature'])
+    ][['id']]
+    return result.rename(columns={'id': 'Id'})
 
 
 print(rising_temperature(weather))
