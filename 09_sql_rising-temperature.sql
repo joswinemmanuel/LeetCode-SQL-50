7b0c6fbe142c3169a79796b66f70851122ac5125
joswinemmanuel <joswinemmanuel2002@gmail.com>
2026-02-23 14:35:19 +0530

    Rising Temperature using SQL

--- 09_sql_rising-temperature.sql
+++ 09_sql_rising-temperature.sql
@@ -1,5 +1,5 @@
-SELECT w2.id AS Id
+SELECT w1.id AS Id
 FROM Weather w1
 JOIN Weather w2
-ON DATEDIFF(w2.recordDate, w1.recordDate) = 1
-WHERE w2.temperature > w1.temperature;
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
